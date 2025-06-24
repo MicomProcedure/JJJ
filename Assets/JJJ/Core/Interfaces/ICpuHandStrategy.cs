04a62c856c84c4c3f8658b32bb667697e44f6edd
Artemol <67518760+Artemol@users.noreply.github.com>
2025-06-24 20:51:29 +0900

    feat: ICpuHandStrategyを追加

---
+++ Assets/JJJ/Core/Interfaces/ICpuHandStrategy.cs
@@ -0,0 +1,17 @@
+using JJJ.Core.Entities;
+
+namespace JJJ.Core.Interfaces
+{
+  /// <summary>
+  /// CPUの手を決定する戦略のインターフェース
+  /// </summary>
+  public interface ICpuHandStrategy
+  {
+    /// <summary>
+    /// CPUの次の手を決定する
+    /// </summary>
+    /// <param name="turnContext">現在のターンのコンテキスト</param>
+    /// <returns>CPUの次の手</returns>
+    public Hand GetNextCpuHand(TurnContext turnContext);
+  }
+}
