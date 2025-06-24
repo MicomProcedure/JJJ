525f86a56d22247e6cab4e7589875a3da6326fc6
Artemol <67518760+Artemol@users.noreply.github.com>
2025-06-24 20:46:15 +0900

    feat: IJudgeServiceを追加

---
+++ Assets/JJJ/Core/Interfaces/IJudgeService.cs
@@ -0,0 +1,18 @@
+using JJJ.Core.Entities;
+
+namespace JJJ.Core.Interfaces
+{
+  /// <summary>
+  /// じゃんけんの結果を判定するサービスのインターフェース
+  /// </summary>
+  public interface IJudgeService
+  {
+    /// <summary>
+    /// じゃんけんの結果を判定する
+    /// </summary>
+    /// <param name="playerHand">プレイヤーの手</param>
+    /// <param name="opponentHand">対戦相手の手</param>
+    /// <returns>じゃんけんの結果</returns>
+    public JudgeResult Judge(Hand playerHand, Hand opponentHand);
+  }
+}
