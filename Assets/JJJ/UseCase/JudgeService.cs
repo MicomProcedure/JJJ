c95ef98bfd86a85bf9eb91d69714fc272f551c65
Artemol <67518760+Artemol@users.noreply.github.com>
2025-09-17 18:17:21 +0900

    feat: JudgeServiceにnamespaceを追加、Start()がIStartableの実装であることを明記

--- Assets/JJJ/UseCase/JudgeService.cs
+++ Assets/JJJ/UseCase/JudgeService.cs
@@ -6,125 +6,129 @@ using JJJ.Core.Interfaces;
 using R3;
 using VContainer.Unity;
 
-public class JudgeService : IDisposable, IStartable
+namespace JJJ.UseCase
 {
-  private IRuleSet ruleSet;
-  private IEnumerable<ICpuHandStrategy> strategies;
-  private ITimerService timerService;
-  private readonly TimeSpan JudgeLimit = TimeSpan.FromSeconds(5);
-  private Observable<Unit> playerWinObservable;
-  private Observable<Unit> opponentWinObservable;
-
-  private CompositeDisposable currentTurnDisposables;
-
-  private enum PlayerClaim
+  public class JudgeService : IDisposable, IStartable
   {
-    PlayerWin,
-    OpponentWin,
-    Timeout
-  }
+    private IRuleSet ruleSet;
+    private IEnumerable<ICpuHandStrategy> strategies;
+    private ITimerService timerService;
+    private readonly TimeSpan JudgeLimit = TimeSpan.FromSeconds(5);
+    private Observable<Unit> playerWinObservable;
+    private Observable<Unit> opponentWinObservable;
 
-  private ICpuHandStrategy currentPlayerStrategy;
-  private ICpuHandStrategy currentOpponentStrategy;
+    private CompositeDisposable currentTurnDisposables;
 
-  public JudgeService(IRuleSet ruleSet, IEnumerable<ICpuHandStrategy> strategies, ITimerService timerService,
-                      Observable<Unit> playerWinObservable, Observable<Unit> opponentWinObservable)
-  {
-    this.ruleSet = ruleSet;
-    this.strategies = strategies;
-    this.timerService = timerService;
-    this.playerWinObservable = playerWinObservable;
-    this.opponentWinObservable = opponentWinObservable;
-  }
-
-  /// <summary>
-  /// 現在のターン情報
-  /// </summary>
-  private TurnContext currentTurnContext;
-
-  public void Start()
-  {
-    currentTurnContext = new TurnContext();
-    StartSession();
-  }
-
-  /// <summary>
-  /// 新しいセッションを開始する
-  /// </summary>
-  public void StartSession()
-  {
-    // ランダムに戦略を選択
-    var rand = new Random();
-    int index = rand.Next(strategies.Count());
-    currentPlayerStrategy = strategies.ElementAt(index);
-    index = rand.Next(strategies.Count());
-    currentOpponentStrategy = strategies.ElementAt(index);
-    currentPlayerStrategy.Initialize();
-    currentOpponentStrategy.Initialize();
-
-    // 新しいターンを開始
-    StartTurn();
-  }
-
-  /// <summary>
-  /// 新しいターンを開始する
-  /// </summary>
-  public void StartTurn()
-  {
-    // 既存ターン用の購読を破棄
-    currentTurnDisposables?.Dispose();
-    currentTurnDisposables = new CompositeDisposable();
-
-    currentTurnContext.NextTurn();
-
-    // CPUの手を決定
-    var playerHand = currentPlayerStrategy.GetNextCpuHand(currentTurnContext);
-    var opponentHand = currentOpponentStrategy.GetNextCpuHand(currentTurnContext);
-    var truthResult = ruleSet.Judge(playerHand, opponentHand, currentTurnContext);
-    // TODO: Viewへ手の情報を通知 (playerHand, opponentHand)
-
-    // プレイヤーのジャッジと時間切れをraceさせる
-    var claimSubject = new Subject<PlayerClaim>();
-    var subscription = claimSubject.Take(1).Subscribe(claim =>
+    private enum PlayerClaim
     {
-      bool isPlayerJudgementCorrect = claim switch
-      {
-        PlayerClaim.PlayerWin => truthResult.Type == JudgeResultType.Win,
-        PlayerClaim.OpponentWin => truthResult.Type == JudgeResultType.Lose,
-        PlayerClaim.Timeout => false,
-        _ => false
-      };
+      PlayerWin,
+      OpponentWin,
+      Timeout
+    }
 
-      // TODO: スコア計算や結果通知 (truthResult, claim, isPlayerJudgementCorrect)
+    private ICpuHandStrategy currentPlayerStrategy;
+    private ICpuHandStrategy currentOpponentStrategy;
 
-      if (truthResult.Type == JudgeResultType.Draw)
-      {
-        // TODO: 後でアニメーションが終わったら進めるように変更
-        Observable.TimerFrame(0)
-          .Subscribe(_ => StartTurn())
-          .AddTo(currentTurnDisposables);
-      }
-      else
-      {
-        // TODO: 後でアニメーションが終わったら進めるように変更
-        Observable.TimerFrame(0)
-          .Subscribe(_ => StartSession())
-          .AddTo(currentTurnDisposables);
-      }
-    });
-    var subPlayer = playerWinObservable.Subscribe(_ => claimSubject.OnNext(PlayerClaim.PlayerWin));
-    var subOpponent = opponentWinObservable.Subscribe(_ => claimSubject.OnNext(PlayerClaim.OpponentWin));
-    var subTimeout = timerService.After(JudgeLimit).Subscribe(_ => claimSubject.OnNext(PlayerClaim.Timeout));
+    public JudgeService(IRuleSet ruleSet, IEnumerable<ICpuHandStrategy> strategies, ITimerService timerService,
+                        Observable<Unit> playerWinObservable, Observable<Unit> opponentWinObservable)
+    {
+      this.ruleSet = ruleSet;
+      this.strategies = strategies;
+      this.timerService = timerService;
+      this.playerWinObservable = playerWinObservable;
+      this.opponentWinObservable = opponentWinObservable;
+    }
 
-    currentTurnDisposables.Add(subscription);
-    currentTurnDisposables.Add(subPlayer);
-    currentTurnDisposables.Add(subOpponent);
-    currentTurnDisposables.Add(subTimeout);
-    currentTurnDisposables.Add(claimSubject);
+    /// <summary>
+    /// 現在のターン情報
+    /// </summary>
+    private TurnContext currentTurnContext;
+
+    void IStartable.Start()
+    {
+      currentTurnContext = new TurnContext();
+      StartSession();
+    }
+
+    /// <summary>
+    /// 新しいセッションを開始する
+    /// </summary>
+    public void StartSession()
+    {
+      // ランダムに戦略を選択
+      var rand = new Random();
+      int index = rand.Next(strategies.Count());
+      currentPlayerStrategy = strategies.ElementAt(index);
+      index = rand.Next(strategies.Count());
+      currentOpponentStrategy = strategies.ElementAt(index);
+      currentPlayerStrategy.Initialize();
+      currentOpponentStrategy.Initialize();
+
+      // 新しいターンを開始
+      StartTurn();
+    }
+
+    /// <summary>
+    /// 新しいターンを開始する
+    /// </summary>
+    public void StartTurn()
+    {
+      // 既存ターン用の購読を破棄
+      currentTurnDisposables?.Dispose();
+      currentTurnDisposables = new CompositeDisposable();
+
+      currentTurnContext.NextTurn();
+
+      // CPUの手を決定
+      var playerHand = currentPlayerStrategy.GetNextCpuHand(currentTurnContext);
+      var opponentHand = currentOpponentStrategy.GetNextCpuHand(currentTurnContext);
+      var truthResult = ruleSet.Judge(playerHand, opponentHand, currentTurnContext);
+      // TODO: Viewへ手の情報を通知 (playerHand, opponentHand)
+
+      // プレイヤーのジャッジと時間切れをraceさせる
+      var claimSubject = new Subject<PlayerClaim>();
+      var subscription = claimSubject.Take(1).Subscribe(claim =>
+      {
+        bool isPlayerJudgementCorrect = claim switch
+        {
+          PlayerClaim.PlayerWin => truthResult.Type == JudgeResultType.Win,
+          PlayerClaim.OpponentWin => truthResult.Type == JudgeResultType.Lose,
+          PlayerClaim.Timeout => false,
+          _ => false
+        };
+
+        // TODO: スコア計算や結果通知 (truthResult, claim, isPlayerJudgementCorrect)
+
+        if (truthResult.Type == JudgeResultType.Draw)
+        {
+          // TODO: 後でアニメーションが終わったら進めるように変更
+          Observable.TimerFrame(0)
+            .Subscribe(_ => StartTurn())
+            .AddTo(currentTurnDisposables);
+        }
+        else
+        {
+          // TODO: 後でアニメーションが終わったら進めるように変更
+          Observable.TimerFrame(0)
+            .Subscribe(_ => StartSession())
+            .AddTo(currentTurnDisposables);
+        }
+      });
+      var subPlayer = playerWinObservable.Subscribe(_ => claimSubject.OnNext(PlayerClaim.PlayerWin));
+      var subOpponent = opponentWinObservable.Subscribe(_ => claimSubject.OnNext(PlayerClaim.OpponentWin));
+      var subTimeout = timerService.After(JudgeLimit).Subscribe(_ => claimSubject.OnNext(PlayerClaim.Timeout));
+
+      currentTurnDisposables.Add(subscription);
+      currentTurnDisposables.Add(subPlayer);
+      currentTurnDisposables.Add(subOpponent);
+      currentTurnDisposables.Add(subTimeout);
+      currentTurnDisposables.Add(claimSubject);
+    }
+
+    public void Dispose()
+    {
+      currentTurnDisposables?.Dispose();
+    }
   }
 
-  public void Dispose()
-  {
-    currentTurnDisposables?.Dispose();
-  }
 }
