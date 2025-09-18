d4962961b1211295324010643a8ac61fc92f7308
Artemol <67518760+Artemol@users.noreply.github.com>
2025-09-19 02:53:53 +0900

    refactor: 乱数を生成する部分をIRandomServiceに分離

--- Assets/JJJ/Infrastructure/CpuHandStrategy/CyclicStrategy.cs
+++ Assets/JJJ/Infrastructure/CpuHandStrategy/CyclicStrategy.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using JJJ.Core.Entities;
 using JJJ.Core.Interfaces;
@@ -18,6 +17,19 @@ namespace JJJ.Infrastructure.CpuHandStrategy
     /// </summary>
     private double ViolationProbability = 0.1;
 
+    /// <summary>
+    /// 乱数生成サービス
+    /// </summary>
+    private readonly IRandomService _randomService;
+
+    public CyclicStrategy(IRandomService randomService)
+    {
+      _randomService = randomService;
+    }
+
+    /// <summary>
+    /// 戦略の初期化
+    /// </summary>
     public void Initialize()
     {
       currentSelectedIndex = -1;
@@ -25,18 +37,16 @@ namespace JJJ.Infrastructure.CpuHandStrategy
 
     public Hand GetNextCpuHand(TurnContext turnContext)
     {
-      // 乱数を生成
-      var rand = new Random();
       var validHandTypes = HandUtil.GetValidHandTypesFromContext(turnContext);
 
       //  反則するかどうかを判定
-      if (rand.NextDouble() < ViolationProbability)
+      if (_randomService.NextDouble() < ViolationProbability)
       {
         // 無効な手のListを取得
         var allHandTypes = HandUtil.AllHandTypes.ToList();
         var invalidHandTypes = allHandTypes.Except(validHandTypes).ToList();
         // 無効な手と後出しをランダムに選択
-        int chosenType = rand.Next(0, invalidHandTypes.Count + 1);
+        int chosenType = _randomService.Next(0, invalidHandTypes.Count + 1);
         if (chosenType < invalidHandTypes.Count)
         {
           // 無効な手を選択
@@ -46,7 +56,7 @@ namespace JJJ.Infrastructure.CpuHandStrategy
         {
           // 後出しを選択
           // 手は有効な手の中からランダムに選択
-          int handType = rand.Next(0, validHandTypes.Count());
+          int handType = _randomService.Next(0, validHandTypes.Count());
           return new Hand(
             validHandTypes.ElementAt(handType),
             HandUtil.GetHandName(validHandTypes.ElementAt(handType)),
@@ -57,7 +67,7 @@ namespace JJJ.Infrastructure.CpuHandStrategy
 
       // 反則しない場合は循環的に手を選択
       // セッションで最初の手ならランダムに選択し、そうでなければ次の手を選択
-      currentSelectedIndex = currentSelectedIndex < 0 ? rand.Next(0, validHandTypes.Count()) : (currentSelectedIndex + 1) % validHandTypes.Count();
+      currentSelectedIndex = currentSelectedIndex < 0 ? _randomService.Next(0, validHandTypes.Count()) : (currentSelectedIndex + 1) % validHandTypes.Count();
       var chosenHandType = validHandTypes.ElementAt(currentSelectedIndex);
       return new Hand(chosenHandType, HandUtil.GetHandName(chosenHandType));
     }
