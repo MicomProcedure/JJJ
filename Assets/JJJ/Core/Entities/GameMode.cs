7daf1a876cfd0253289382f6ff27e36f4836f1c4
Artemol <67518760+Artemol@users.noreply.github.com>
2025-09-21 01:51:51 +0900

    feat: Core.EntitiesにGameModeを追加

---
+++ Assets/JJJ/Core/Entities/GameMode.cs
@@ -0,0 +1,20 @@
+namespace JJJ.Core.Entities
+{
+  public enum GameMode
+  {
+    /// <summary>
+    /// イージーモード
+    /// </summary>
+    Easy,
+
+    /// <summary>
+    /// ノーマルモード
+    /// </summary>
+    Normal,
+
+    /// <summary>
+    /// ハードモード
+    /// </summary>
+    Hard
+  }
+}
