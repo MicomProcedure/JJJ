8a5a1663746e0a2308b93a93dacd31dd6af62bd4
Artemol <67518760+Artemol@users.noreply.github.com>
2025-09-26 19:34:24 +0900

    feat: CompositeHandAnimationPresenterにすべての手をReset/Initする関数を追加

--- Assets/JJJ/Core/Interfaces/ICompositeHandAnimationPresenter.cs
+++ Assets/JJJ/Core/Interfaces/ICompositeHandAnimationPresenter.cs
@@ -14,5 +14,15 @@ namespace JJJ.Core.Interfaces
     /// 対戦相手の手のアニメーションプレゼンター
     /// </summary>
     public IHandAnimationPresenter OpponentHandAnimationPresenter { get; }
+
+    /// <summary>
+    /// 両者の手のアニメーションをリセットする
+    /// </summary>
+    public void ResetHandAll();
+
+    /// <summary>
+    /// 両者の手のアニメーションを初期位置に戻す
+    /// </summary>
+    public void ReturnInitAll();
   }
 }
--- Assets/JJJ/View/Scripts/CompositeHandAnimationPresenter.cs
+++ Assets/JJJ/View/Scripts/CompositeHandAnimationPresenter.cs
@@ -10,5 +10,19 @@ namespace JJJ.View
 
     public IHandAnimationPresenter PlayerHandAnimationPresenter => _playerHandAnimationPresenter;
     public IHandAnimationPresenter OpponentHandAnimationPresenter => _opponentHandAnimationPresenter;
+
+    public void ResetHandAll()
+    {
+      Debug.Log("ResetHandAll");
+      PlayerHandAnimationPresenter.ResetHand();
+      OpponentHandAnimationPresenter.ResetHand();
+    }
+
+    public void ReturnInitAll()
+    {
+      Debug.Log("ReturnInitAll");
+      PlayerHandAnimationPresenter.ReturnInit();
+      OpponentHandAnimationPresenter.ReturnInit();
+    }
   }
 }
