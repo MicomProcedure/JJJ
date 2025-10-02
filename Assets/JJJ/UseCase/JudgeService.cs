8beb9a37db98ebe41a20090bb5110f07e372930e
Artemol <67518760+Artemol@users.noreply.github.com>
2025-10-02 15:30:04 +0900

    feat: JudgeServiceにスコア計算機能を追加

--- Assets/JJJ/UseCase/JudgeService.cs
+++ Assets/JJJ/UseCase/JudgeService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using JJJ.Core.Entities;
 using JJJ.Core.Interfaces;
 using JJJ.Utils;
+using JJJ.View;
 using R3;
 using VContainer.Unity;
 using ZLogger;
@@ -17,6 +18,8 @@ namespace JJJ.UseCase
     private readonly TimeSpan _judgeLimit = TimeSpan.FromSeconds(5);
     private readonly ICompositeHandAnimationPresenter _compositeHandAnimationPresenter;
     private readonly ITimerRemainsPresenter _timerRemainsPresenter;
+    private readonly IScoreCalculator _scoreCalculator;
+    private readonly CurrentScorePresenter _currentScorePresenter;
     private readonly IJudgeInput _judgeInput;
 
     private CompositeDisposable _currentTurnDisposables = new CompositeDisposable();
@@ -31,6 +34,11 @@ namespace JJJ.UseCase
     /// </summary>
     private TurnContext? _currentTurnContext = null;
 
+    /// <summary>
+    /// 現在のスコア
+    /// </summary>
+    private int _currentScore = 0;
+
     private readonly Microsoft.Extensions.Logging.ILogger _logger = LogManager.CreateLogger<JudgeService>();
 
     public JudgeService(IRuleSet ruleSet,
@@ -39,6 +47,8 @@ namespace JJJ.UseCase
                         IJudgeInput judgeInput,
                         ICompositeHandAnimationPresenter compositeHandAnimationPresenter,
                         ITimerRemainsPresenter timerRemainsPresenter,
+                        IScoreCalculator scoreCalculator,
+                        CurrentScorePresenter currentScorePresenter,
                         IStrategySelector strategySelector,
                         ITurnExecutor turnExecutor)
     {
@@ -47,6 +57,8 @@ namespace JJJ.UseCase
       _timerService = timerService;
       _compositeHandAnimationPresenter = compositeHandAnimationPresenter;
       _timerRemainsPresenter = timerRemainsPresenter;
+      _scoreCalculator = scoreCalculator;
+      _currentScorePresenter = currentScorePresenter;
       _judgeInput = judgeInput;
       _strategySelector = strategySelector;
       _turnExecutor = turnExecutor;
@@ -56,6 +68,8 @@ namespace JJJ.UseCase
     {
       _logger.ZLogTrace($"JudgeService: Start");
       _currentTurnContext = new TurnContext();
+      _currentScore = 0;
+      _currentScorePresenter.SetCurrentScore(_currentScore);
       StartSession();
     }
 
@@ -97,6 +111,16 @@ namespace JJJ.UseCase
         .Subscribe(outcome =>
         {
           // TODO: Viewへ手・結果通知（outcome.TruthResult, outcome.Claim, outcome.IsPlayerJudgementCorrect）
+          _logger.ZLogTrace($"JudgeService: TurnOutcome - Truth: {outcome.TruthResult.Type}, Claim: {outcome.Claim}, Correct: {outcome.IsPlayerJudgementCorrect}, JudgeTime: {outcome.JudgeTime}");
+
+          // 点数を加算
+          int scoreDiff = _scoreCalculator.CalculateScore(outcome.IsPlayerJudgementCorrect, outcome.JudgeTime);
+          _currentScore += scoreDiff;
+          if (_currentScore < 0) _currentScore = 0;
+          _currentScorePresenter.SetCurrentScore(_currentScore);
+          _currentScorePresenter.SetScoreDiff(scoreDiff);
+
+          // 引き分けならターン継続、勝敗がついたらセッション再開
           if (outcome.TruthResult.Type == JudgeResultType.Draw)
           {
             _logger.ZLogTrace($"JudgeService: Draw - Restart Turn");
