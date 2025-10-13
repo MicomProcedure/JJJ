f210885384c4c61ea635eb1daebf9c8367fcb928
kamekiti <ishi.y0331kamekiti@gmail.com>
2025-10-14 06:55:38 +0900

    feat: タイトル画面のボタンに機能を追加

---
+++ Assets/JJJ/UI/Scripts/TitleButtonObservables.cs
@@ -0,0 +1,25 @@
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace JJJ.UI
+{
+  public class TitleButtonObservables : MonoBehaviour
+  {
+    [SerializeField] private Button _easyButton = null!;
+    [SerializeField] private Button _normalButton = null!;
+    [SerializeField] private Button _hardButton = null!;
+    [SerializeField] private Button _exitButton = null!;
+    [SerializeField] private Button _optionButton = null!;
+    [SerializeField] private Button _helpButton = null!;
+    [SerializeField] private Button _rankingButton = null!;
+
+    public Observable<Unit> EasyButtonOnClick => _easyButton.OnClickAsObservable();
+    public Observable<Unit> NormalButtonOnClick => _normalButton.OnClickAsObservable();
+    public Observable<Unit> HardButtonOnClick => _hardButton.OnClickAsObservable();
+    public Observable<Unit> ExitButtonOnClick => _exitButton.OnClickAsObservable();
+    public Observable<Unit> OptionButtonOnClick => _optionButton.OnClickAsObservable();
+    public Observable<Unit> HelpButtonOnClick => _helpButton.OnClickAsObservable();
+    public Observable<Unit> RankingButtonOnClick => _rankingButton.OnClickAsObservable();
+  }
+}
