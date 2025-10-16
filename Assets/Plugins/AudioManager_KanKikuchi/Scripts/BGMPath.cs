4551193419bccaff1a2e6721f158d3a379352d28
kamekiti <ishi.y0331kamekiti@gmail.com>
2025-10-17 03:17:41 +0900

    feat: BGM, SEを追加

--- Assets/Plugins/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ Assets/Plugins/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,6 +5,9 @@
 /// </summary>
 public static class BGMPath{
 
+	public const string BGM1 = "BGM/BGM1";
+	public const string BGM2 = "BGM/BGM2";
+	public const string BGM3 = "BGM/BGM3";
 
 }
 
--- Assets/Plugins/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/Plugins/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,6 +5,12 @@
 /// </summary>
 public static class SEPath{
 
+	public const string SE1 = "SE/SE1";
+	public const string SE2 = "SE/SE2";
+	public const string SE3 = "SE/SE3";
+	public const string SE4 = "SE/SE4";
+	public const string SE5 = "SE/SE5";
+	public const string SE6 = "SE/SE6";
 
 }
 
