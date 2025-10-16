e283a681f32c2d187ccca1daefee38291fd370f3
kamekiti <ishi.y0331kamekiti@gmail.com>
2025-10-16 09:33:57 +0900

    feat: ButtonSoundEffectorを追加

---
+++ Assets/JJJ/UI/Scripts/ButtonSoundEffector.cs
@@ -0,0 +1,28 @@
+using KanKikuchi.AudioManager;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace JJJ.UI
+{
+  public class ButtonSoundEffector : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+  {
+    [SerializeField, SEPathSelector] private string _hoverSE = "";
+    [SerializeField, SEPathSelector] private string _clickSE = "";
+
+    public void OnPointerEnter(PointerEventData _)
+    {
+      if (!string.IsNullOrEmpty(_hoverSE))
+      {
+        SEManager.Instance.Play(_hoverSE);
+      }
+    }
+
+    public void OnPointerClick(PointerEventData _)
+    {
+      if (!string.IsNullOrEmpty(_clickSE))
+      {
+        SEManager.Instance.Play(_clickSE);
+      }
+    }
+  }
+}
