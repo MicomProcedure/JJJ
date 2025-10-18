e1809ecc57826138bd5f40e45547ca8fa4af68da
kamekiti <ishi.y0331kamekiti@gmail.com>
2025-10-18 18:46:31 +0900

    feat: リザルト画面からタイトル画面に遷移する機能を追加

--- Assets/JJJ/DI/ResultLifetimeScope.cs
+++ Assets/JJJ/DI/ResultLifetimeScope.cs
@@ -3,18 +3,23 @@ using VContainer;
 using VContainer.Unity;
 using UnityEngine;
 using JJJ.View;
+using JJJ.UI;
 
 namespace JJJ.DI
 {
   public sealed class ResultLifetimeScope : LifetimeScope
   {
-    [SerializeField] private ResultView _resultView;
+    [SerializeField] private ResultView _resultView = null!;
+    [SerializeField] private ResultButtonObservales _resultButtonObservales = null!;
 
     protected override void Configure(IContainerBuilder builder)
     {
       builder.RegisterSceneLifecycle<ResultSceneLifecycle>();
 
+      builder.RegisterEntryPoint<ResultButtonManager>(Lifetime.Scoped);
+
       builder.RegisterComponent(_resultView);
+      builder.RegisterComponent(_resultButtonObservales);
     }
   }
 }
