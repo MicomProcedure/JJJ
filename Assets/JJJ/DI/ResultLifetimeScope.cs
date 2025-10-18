9393f8f2b84a84a1da6688e25f29630c0186229e
kamekiti <ishi.y0331kamekiti@gmail.com>
2025-10-18 21:11:07 +0900

    feat: リザルトに「画面をクリックしてください」の表示を追加

--- Assets/JJJ/DI/ResultLifetimeScope.cs
+++ Assets/JJJ/DI/ResultLifetimeScope.cs
@@ -4,12 +4,14 @@ using VContainer.Unity;
 using UnityEngine;
 using JJJ.View;
 using JJJ.UI;
+using TMPro;
 
 namespace JJJ.DI
 {
   public sealed class ResultLifetimeScope : LifetimeScope
   {
     [SerializeField] private ResultView _resultView = null!;
+    [SerializeField] private TextMeshProUGUI _clickScreenText = null!;
     [SerializeField] private ResultButtonObservales _resultButtonObservales = null!;
 
     protected override void Configure(IContainerBuilder builder)
@@ -20,6 +22,8 @@ namespace JJJ.DI
 
       builder.RegisterComponent(_resultView);
       builder.RegisterComponent(_resultButtonObservales);
+
+      builder.RegisterInstance(_clickScreenText.gameObject).Keyed("ClickScreenText");
     }
   }
 }
--- Assets/JJJ/UI/Scripts/ResultButtonManager.cs
+++ Assets/JJJ/UI/Scripts/ResultButtonManager.cs
@@ -1,8 +1,11 @@
 using System;
 using JJJ.Infrastructure;
 using JJJ.Utils;
+using KanKikuchi.AudioManager;
 using MackySoft.Navigathena.SceneManagement;
 using R3;
+using UnityEngine;
+using VContainer;
 using VContainer.Unity;
 
 namespace JJJ.UI
@@ -10,20 +13,25 @@ namespace JJJ.UI
   public class ResultButtonManager : IStartable, IDisposable
   {
     private ResultButtonObservales _resultButtonObservales;
+    private GameObject _clickScreenText;
 
     private CompositeDisposable _disposables = new();
 
-    public ResultButtonManager(ResultButtonObservales resultButtonObservales)
+    public ResultButtonManager(ResultButtonObservales resultButtonObservales,
+                               [Key("ClickScreenText")] GameObject clickScreenText)
     {
       _resultButtonObservales = resultButtonObservales;
+      _clickScreenText = clickScreenText;
     }
 
     public void Start()
     {
       _resultButtonObservales.BackgroundButtonOnClick
-        .DelaySubscription(TimeSpan.FromSeconds(1))
+        .DelaySubscription(TimeSpan.FromSeconds(3))
+        .Do(onSubscribe: () => _clickScreenText.SetActive(true))
         .Subscribe(async _ =>
         {
+          SEManager.Instance.Play(SEPath.SE6);
           await GlobalSceneNavigator.Instance.Push(SceneNavigationUtil.TitleSceneIdentifier, new FadeTransitionDirector(SceneNavigationUtil.FadeTransitionIdentifier));
         })
         .AddTo(_disposables);
