8dee9c9ff6c6c24ec246498f36e73fedf129e670
kamekiti <ishi.y0331kamekiti@gmail.com>
2025-10-19 21:16:27 +0900

    feat: ルール表示機能を追加

--- Assets/JJJ/UI/Scripts/GameButtonObservables.cs
+++ Assets/JJJ/UI/Scripts/GameButtonObservables.cs
@@ -8,8 +8,10 @@ namespace JJJ.UI
   {
     [SerializeField] private Button _exitButton = null!;
     [SerializeField] private Button _ruleButton = null!;
+    [SerializeField] private Button _hideRuleButton = null!;
 
     public Observable<Unit> ExitButtonOnClick => _exitButton.OnClickAsObservable();
     public Observable<Unit> RuleButtonOnClick => _ruleButton.OnClickAsObservable();
+    public Observable<Unit> HideRuleButtonOnClick => _hideRuleButton.OnClickAsObservable();
   }
 }
