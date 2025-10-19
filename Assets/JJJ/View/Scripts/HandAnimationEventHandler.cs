821c886a5c26c48efb93b24bb683c509efd06758
shirokuro419087 <kazuki419087@gmail.com>
2025-10-19 23:09:59 +0900

    feat: 後出し時に振り上げ後に静止するようのアニメーションを変更

--- Assets/JJJ/View/Scripts/HandAnimationEventHandler.cs
+++ Assets/JJJ/View/Scripts/HandAnimationEventHandler.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using ZLogger;
+using JJJ.Utils;
+using Cysharp.Threading.Tasks;
 
 namespace JJJ.View
 {
     public class HandAnimationEventHandler : MonoBehaviour
     {
+        [SerializeField] private HandAnimationPresenter _handAnimationPresenter;
         [SerializeField] private GameObject _balance = null!;
         [SerializeField] private GameObject _ofuda = null!;
+
+        private readonly Microsoft.Extensions.Logging.ILogger _logger = LogManager.CreateLogger<HandAnimationEventHandler>();
 
         public void OnPlayBalance()
         {
@@ -21,6 +27,17 @@ namespace JJJ.View
         {
             _balance.SetActive(false);
             _ofuda.SetActive(false);
+        }
+
+        public void OnIsTimeout()
+        {
+            if (_handAnimationPresenter == null)
+            {
+                _logger.ZLogError($"HandAnimationPresenter is not assigned in HandAnimationEventHandler.");
+                return;
+            }
+            
+            _handAnimationPresenter.DoTimeout().Forget();
         }
     }
 }
