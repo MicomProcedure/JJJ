84b32f77a336ab5c6852a2caec8c1449c492ed26
Artemol <67518760+Artemol@users.noreply.github.com>
2025-10-31 03:57:57 +0900

    feat: ButtonSoundEffectorをIPointerClickからIPointerDownに変更

--- Assets/JJJ/UI/Scripts/ButtonSoundEffector.cs
+++ Assets/JJJ/UI/Scripts/ButtonSoundEffector.cs
@@ -1,27 +1,37 @@
 using KanKikuchi.AudioManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace JJJ.UI
 {
   /// <summary>
   /// ボタンをクリックしたときにSEを再生するコンポーネント
   /// </summary>
-  public class ButtonSoundEffector : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+  public class ButtonSoundEffector : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
   {
     [SerializeField, SEPathSelector] private string _hoverSE = "";
     [SerializeField, SEPathSelector] private string _clickSE = "";
 
+    private Button? _button;
+
+    private void Awake()
+    {
+      TryGetComponent<Button>(out _button);
+    }
+
     public void OnPointerEnter(PointerEventData _)
     {
+      if (_button != null && !_button.IsInteractable()) return;
       if (!string.IsNullOrEmpty(_hoverSE))
       {
         SEManager.Instance.Play(_hoverSE);
       }
     }
 
-    public void OnPointerClick(PointerEventData _)
+    public void OnPointerDown(PointerEventData _)
     {
+      if (_button != null && !_button.IsInteractable()) return;
       if (!string.IsNullOrEmpty(_clickSE))
       {
         SEManager.Instance.Play(_clickSE);
